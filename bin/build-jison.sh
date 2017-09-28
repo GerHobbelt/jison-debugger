8971cc92a616ada59a5038d2b3ad5888cf519600
Ger Hobbelt <ger@hobbelt.com>
2017-09-28 23:00:43 +0200

    make jison build process run again on Windows/bash + DO NOT ignore the produced JS file: track the browserify output for jison itself in git.

--- bin/build-jison.sh
+++ bin/build-jison.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # build up the version of jison.js used in the web worker
-cd node_modules/jison
-npm install
-cd -
-./node_modules/.bin/browserify --exports require node_modules/jison/entry.js > app/scripts/worker/jison.js
+#cd node_modules/gho-jison
+#npm install
+#cd -
+./node_modules/.bin/browserify --exports require node_modules/jison-gho/entry.js > app/scripts/worker/jison.js
